f7e84485df26c27e8e8d8ba821b1b92582ce1e44
Christy Drain <christy.drain@kainos.com>
2022-11-22 14:06:01 +0000

    Test fir protected branch

--- src/main/java/com/kainos/ea/trueApplication.java
+++ src/main/java/com/kainos/ea/trueApplication.java
@@ -2,54 +2,17 @@ package com.kainos.ea;
 
 
 import com.kainos.ea.controller.WebService;
-import com.kainos.ea.contructors.Employee.Employee;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.kainos.ea.database.DatabaseConnection.getConnection;
-
-
 public class trueApplication extends Application<trueConfiguration> {
-
-    public static List<Employee> getEmployees() throws SQLException {
-        Connection c = getConnection();
-        Statement st = c.createStatement();
-        ResultSet rs = st.executeQuery(
-                "SELECT * "
-                        + "FROM Employee;");
-
-        List<Employee> employees = new ArrayList<>();
-        while (rs.next()) {
-            Employee employee = new Employee(
-                    rs.getInt("id"),
-                    rs.getString("fname"),
-                    rs.getString("lname"),
-                    rs.getString("NIN"),
-                    rs.getString("bank_account"),
-                    rs.getString("address"),
-                    rs.getFloat("salary")
-                    //rs.get("Salary")
-            );
-            employees.add(employee);
-        }
-        return employees;
-    }
 
     public static void main(final String[] args) throws Exception {
         new trueApplication().run(args);
 
-    }
-
-    @Override
-    public String getName() {
-        return "true";
     }
 
     @Override
